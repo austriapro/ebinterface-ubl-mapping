2287abcbeb6aef56854d05396334eea654e6200f
Philip Helger <philip@helger.com>
2018-01-05 10:36:25 +0100

    Bumped to 2018

--- src/main/java/com/helger/ebinterface/ubl/from/invoice/ICustomInvoiceToEbInterface42Converter.java
+++ src/main/java/com/helger/ebinterface/ubl/from/invoice/ICustomInvoiceToEbInterface42Converter.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2010-2015 Bundesrechenzentrum GmbH - www.brz.gv.at
- * Copyright (c) 2015-2017 AUSTRIAPRO - www.austriapro.at
+ * Copyright (c) 2015-2018 AUSTRIAPRO - www.austriapro.at
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
