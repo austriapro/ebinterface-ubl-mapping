bd0de5c421cc24d1c6bbd93bd566acc5b9beda27
Philip Helger <philip@helger.com>
2021-01-01 13:42:45 +0100

    Updated to 2021

--- src/main/java/at/austriapro/ebinterface/ubl/from/AbstractToEbInterface50Converter.java
+++ src/main/java/at/austriapro/ebinterface/ubl/from/AbstractToEbInterface50Converter.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2010-2015 Bundesrechenzentrum GmbH - www.brz.gv.at
- * Copyright (c) 2015-2020 AUSTRIAPRO - www.austriapro.at
+ * Copyright (c) 2015-2021 AUSTRIAPRO - www.austriapro.at
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
